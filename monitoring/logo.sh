1d8d0f69922107ca4ed57b42cee27323b51d0c2a
breskulpeak <71060373+Boblev999@users.noreply.github.com>
2025-02-27 22:26:23 +0200

    Update logo.sh

--- monitoring/logo.sh
+++ monitoring/logo.sh
@@ -1,6 +1,6 @@
 echo -e "\033[0;34m"
 echo "     .o8                                   oooo                    oooo                                 oooo ";
-echo "    "888                                   `888                    `888                                 `888     ";
+echo "     888                                   `888                    `888                                 `888     ";
 echo "     888oooo.  oooo d8b  .ooooo.   .oooo.o  888  oooo  oooo  oooo   888  oo.ooooo.   .ooooo.   .oooo.    888  oooo     ";
 echo "     d88' `88b `888""8P d88' `88b d88(  "8  888 .8P'   `888  `888   888   888' `88b d88' `88b `P  )88b   888 .8P'     ";
 echo "     888   888  888     888ooo888 `"Y88b.   888888.     888   888   888   888   888 888ooo888  .oP"888   888888.     ";
