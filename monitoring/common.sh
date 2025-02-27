82e47e3a0c055e9aed723c79bb6cc324247c7d62
breskulpeak <71060373+Boblev999@users.noreply.github.com>
2025-02-27 21:36:59 +0200

    Update common.sh

--- monitoring/common.sh
+++ monitoring/common.sh
@@ -1,61 +1,37 @@
-# Color variables
-GREEN="\e[1;32m"    # Green color
-RED="\e[1;31m"      # Red color
-BLUE="\e[0;34m"     # Blue color
-YELLOW="\e[1;33m"   # Yellow color
-NC="\e[0m"          # No color (reset)
+GREEN="\e[1m\e[1;32m" # green color
+RED="\e[1m\e[1;31m" # red color
+BLUE='\033[0;34m'   # blue color
+YELLOW="\e[1m\e[1;33m"  # yellow color
+NC="\e[0m"          # no color
 
-# Function to print the logo
 function printLogo {
-  # Download and execute the logo script from GitHub
-  if curl -s https://raw.githubusercontent.com/Boblev999/arhiv/refs/heads/main/monitoring/logo.sh | bash; then
-    echo -e "${GREEN}Logo printed successfully.${NC}"  # Success message
-  else
-    echo -e "${RED}Failed to print logo.${NC}"        # Error message
-  fi
+  bash <(curl -s https://raw.githubusercontent.com/Boblev999/arhiv/refs/heads/main/monitoring/logo.sh)
 }
 
-# Function to print a line of dashes
 function printLine {
   echo "------------------------------------------------------------------------------------"
 }
 
-# Functions to print colored text
 function printGreen {
-  printf "${GREEN}%s${NC}\n" "$1"  # Print text in green
+  echo -e "${GREEN}${1}${NC}"
 }
+
 function printRed {
-  printf "${RED}%s${NC}\n" "$1"    # Print text in red
+  echo -e "${RED}${1}${NC}"
 }
+
 function printBlue {
-  printf "${BLUE}%s${NC}\n" "$1"   # Print text in blue
+  echo -e "${BLUE}${1}${NC}"
 }
+
 function printYellow {
-  printf "${YELLOW}%s${NC}\n" "$1" # Print text in yellow
+  echo -e "${YELLOW}${1}${NC}"
 }
 
-# Function to add a path to the PATH environment variable
 function addToPath {
-  # Check if the .bash_profile file exists; create it if it doesn't
-  if [ ! -f "$HOME/.bash_profile" ]; then
-    touch "$HOME/.bash_profile"
-  fi
-
-  # Check if the path already exists in .bash_profile
-  if ! grep -qxF "export PATH=\$PATH:${1}" "$HOME/.bash_profile"; then
-    # Add the path to .bash_profile and source the file
-    echo "export PATH=\$PATH:${1}" >> "$HOME/.bash_profile"
-    source "$HOME/.bash_profile"
-    printGreen "Path '${1}' added to \$PATH and .bash_profile sourced."
-  else
-    # Notify if the path already exists
-    printYellow "Path '${1}' already exists in \$PATH."
+  source $HOME/.bash_profile
+  PATH_EXIST=$(grep "${1}" $HOME/.bash_profile)
+  if [ -z "$PATH_EXIST" ]; then
+    echo "export PATH=\$PATH:${1}" >> $HOME/.bash_profile
   fi
 }
-
-# Example usage of functions
-# printLogo
-# printLine
-# printGreen "This is a green message."
-# printRed "This is a red message."
-# addToPath "/usr/local/bin"
