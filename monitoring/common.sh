bffb6df965d3f25cbf5bd33075dbc464fc76cf73
breskulpeak <71060373+Boblev999@users.noreply.github.com>
2025-02-27 23:38:26 +0200

    Update common.sh

--- monitoring/common.sh
+++ monitoring/common.sh
@@ -1,48 +1,33 @@
-# Колірні константи
-GREEN="\e[1m\e[1;32m" # зелений колір
-RED="\e[1m\e[1;31m"   # червоний колір
-BLUE='\033[0;34m'     # синій колір
-YELLOW="\e[1m\e[1;33m" # жовтий колір
-NC="\e[0m"            # без кольору
+GREEN="\e[1m\e[1;32m" # green color
+RED="\e[1m\e[1;31m" # red color
+BLUE='\033[0;34m'   # blue color
+YELLOW="\e[1m\e[1;33m"  # yellow color
+NC="\e[0m"          # no color
 
-# Функція для виведення логотипу (через curl)
 function printLogo {
-  echo "Завантаження logo.sh..."
-  TEMP_FILE=$(mktemp)
-  if ! curl -s https://raw.githubusercontent.com/Boblev999/arhiv/refs/heads/main/monitoring/logo.sh > "$TEMP_FILE"; then
-    echo "Помилка: Не вдалося завантажити logo.sh."
-    return 1
-  fi
-  echo "Виконання logo.sh..."
-  bash "$TEMP_FILE"
-  rm -f "$TEMP_FILE"
+  bash <(https://raw.githubusercontent.com/Boblev999/arhiv/refs/heads/main/monitoring/logo.sh)
 }
-# Функція для виведення лінії
+
 function printLine {
   echo "------------------------------------------------------------------------------------"
 }
 
-# Функція для виведення тексту зеленим кольором
 function printGreen {
   echo -e "${GREEN}${1}${NC}"
 }
 
-# Функція для виведення тексту червоним кольором
 function printRed {
   echo -e "${RED}${1}${NC}"
 }
 
-# Функція для виведення тексту синім кольором
 function printBlue {
   echo -e "${BLUE}${1}${NC}"
 }
 
-# Функція для виведення тексту жовтим кольором
 function printYellow {
   echo -e "${YELLOW}${1}${NC}"
 }
 
-# Функція для додавання шляху до PATH
 function addToPath {
   source $HOME/.bash_profile
   PATH_EXIST=$(grep "${1}" $HOME/.bash_profile)
