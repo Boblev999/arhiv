e7ac18e4745284cbeb7f46a0308a5f47e7a84d7a
breskulpeak <71060373+Boblev999@users.noreply.github.com>
2025-02-27 21:12:02 +0200

    Update common.sh

--- monitoring/common.sh
+++ monitoring/common.sh
@@ -5,7 +5,7 @@ YELLOW="\e[1m\e[1;33m"  # yellow color
 NC="\e[0m"          # no color
 
 function printLogo {
-  bash <(curl -s https://raw.githubusercontent.com/Boblev999/arhiv/refs/heads/main/monitoring/logo.sh)
+  curl -s https://raw.githubusercontent.com/Boblev999/arhiv/refs/heads/main/monitoring/logo.sh | bash
 }
 
 function printLine {
