40762eeb2cb8e48a30868027d213358c4bd0ead0
breskulpeak <71060373+Boblev999@users.noreply.github.com>
2025-02-27 20:45:52 +0200

    Create logo.sh

---
+++ monitoring/logo.sh
@@ -0,0 +1,13 @@
+echo -e "\033[0;34m"
+echo "     .o8                                   oooo                    oooo                                 oooo ";
+echo "    "888                                   `888                    `888                                 `888     ";
+echo "     888oooo.  oooo d8b  .ooooo.   .oooo.o  888  oooo  oooo  oooo   888  oo.ooooo.   .ooooo.   .oooo.    888  oooo     ";
+echo "     d88' `88b `888""8P d88' `88b d88(  "8  888 .8P'   `888  `888   888   888' `88b d88' `88b `P  )88b   888 .8P'     ";
+echo "     888   888  888     888ooo888 `"Y88b.   888888.     888   888   888   888   888 888ooo888  .oP"888   888888.     ";
+echo "     888   888  888     888    .o o.  )88b  888 `88b.   888   888   888   888   888 888    .o d8(  888   888 `88b.     ";
+echo "     `Y8bod8P' d888b    `Y8bod8P' 8""888P' o888o o888o  `V88V"V8P' o888o  888bod8P' `Y8bod8P' `Y888""8o o888o o888o     ";
+echo "                                                                          888     ";
+echo "                                                                         o888o     ";
+echo -e "\033[0;32m"
+echo "                                                                          breskulpeak.com"
+echo -e "\e[0m"
